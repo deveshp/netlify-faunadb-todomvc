857dc33b472ab2a529e1b5622acc7f05987a4400
Chris Anderson <chris@fauna.com>
2018-10-30 13:49:57 -0700

    log better

--- functions/identity-signup/identity-signup.js
+++ functions/identity-signup/identity-signup.js
@@ -6,10 +6,10 @@ function handler(event, context, callback) {
   isNaN(faunadb);
   var payload = JSON.parse(event.body);
   var user = payload.user;
-  console.log("Netlify user " + user + " FAUNADB_SERVER_SECRET " + process.env.FAUNADB_SERVER_SECRET);
+  console.log("Netlify user " + JSON.stringify(user) + " process.env FAUNADB_SERVER_SECRET " + JSON.stringify(process.env));
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ app_metadata: { test: "surely" } })
+    body: JSON.stringify({ app_metadata: { test: "surely too" } })
   });
 }
 module.exports = {handler: handler};
